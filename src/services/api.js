abf506594fc5c3b38d9e5daaf83c80e2d183e9fc
AlecsanderFarias <alecs@devfarias.com>
2021-09-11 16:52:17 -0300

    fix api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https:/sagitarius-bank.herokuapp.com/",
+  baseURL: "https:/sagitarius-bank.herokuapp.com",
 });
 
 export default api;
